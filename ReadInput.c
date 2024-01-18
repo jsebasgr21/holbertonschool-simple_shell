d1c1b7c322fe2cb390def4efc71d772fae401959
jsebasgr21 <jsebasgr21@hotmail.com>
2024-01-18 00:39:47 -0500

    remove the \n character

--- ReadInput.c
+++ ReadInput.c
@@ -6,11 +6,25 @@
 
 int ReadInput()
 {
-char *input = NULL;
+char *uinput = NULL;
 size_t bufsize = 0;
 int size;
 
-size = getline(&input, &bufsize, stdin);
+size = getline(&uinput, &bufsize, stdin);
 
+if (size == -1){
+if (feof(stdin))
+{
+free(uinput);
+exit(0);
+}
+else
+{
+perror("ERRROR TO READ INPUT");
+free(uinput);
+}
+}
+uinput[strcspn(uinput, "\n")] = '\0';
+free(uinput);
 return (size);
 }
